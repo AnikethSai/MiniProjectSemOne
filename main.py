6ea566d947fea4adc4c0af739d4787eacaff86e6
Aniketh Sai <71541452+AnikethSai@users.noreply.github.com>
2025-01-07 17:01:51 +0530

    fixed final_bal()

--- main.py
+++ main.py
@@ -1,55 +1,74 @@
-#payer: expen[i]['payer']
+# payer: expSheet[i]['payer']
+'''
+p1 -> stroes the main datasheet dictionary
 
-peo =[]
-p1={}
-n=int(input('Enter the number of people '))
-expen={}
-exppay={}
+'''
+peo = []
+p1 = {}
+n = int(input('Enter the number of people '))
+expSheet = {}
 def people():
     for i in range(n):
-        name=input('Enter name: ')
+        name = input('Enter name: ')
         peo.append(name)
-        p1[name]={}
+        p1[name] = {}
     return p1
-
 def dataSheet():
     for i in range(n):
-        p2=peo[:]
+        p2 = peo[:]
         p2.remove(peo[i])
-        #print(p2)
         for j in range(len(p2)):
-            p1[peo[i]][p2[j]]=None
+            p1[peo[i]][p2[j]] = 0
     return p1
-
 def expenses():
-    n=int(input('Enter the number of expenses: '))
-    for i in range(n):
-        global exp
-        exp=input('Enter the name of the expense: ')
-        expamt=int(input('Enter the expense amount: '))
-        payer=input('Enter the payer\'s name: ')
-        expen[exp] = {'amount': expamt, 'payer': payer}
-        global expenses
-        expenses=list(expen.keys())
-        #print(expen)
-        #exppay[exp]=payer
-    print(expen)
-   # print(exppay)
+    n1 = int(input('Enter the number of expenses: '))
+    for i in range(n1):
+        global expname
+        expname = input('Enter the name of the expense: ')
+        expamt = int(input('Enter the expense amount: '))
+        payer = input('Enter the payer\'s name: ')
 
+        if payer not in peo:
+            print("Invalid payer name; retry")
+            continue
+
+        expSheet[expname] = {'amount': expamt, 'payer': payer}
+        print('Expenses entered: \n')
+        for k, v in expSheet.items():
+            print(f"  {k}: {v}")
+        global expense
+        expense = list(expSheet.keys())
 def split():
-    for i in expenses:
-        part=expen[i]['amount']/n 
-        global p1k
-        p1k = list(p1[expen[i]['payer']].keys())
-        p1[expen[i]['payer']][p1k[0]]=part
-        
-        print('Final: ',expen)        
-        print(part)
+    for i in expense:
+        part = expSheet[i]['amount']/n
+        print(f"\nSplitting expense '{expname}' of amount {expSheet[i]['amount']:.2f}:")
+        payer = expSheet[i]['payer']
+        for person in peo:
+            if person != payer:
+                p1[payer][person] += part
+                p1[person][payer] -= part
+                print(f"  {person} owes {payer} {part:.2f}")
+
+
+def final_bal():
+    print("\nFinal Balances:")
+    done = set()
+    for person in peo:
+        for other in peo:
+            #
+            if person!=other:
+                '''print('p',person)
+                print('o',other)'''
+                if p1[person][other] > p1[other][person]:
+                    print(other, ' owes ',  person,' ',p1[person][other])
+                else:
+                    print(person,' owes ',other,' ',p1[other][person])
 
 people()
-a=dataSheet()
+a = dataSheet()
 expenses()
 split()
+final_bal()
 print(a)
-        
-        
+
+
